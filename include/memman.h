db227e1345b6ab9b3f7ca2392ff868d3f058e723
flyingpig233-cpu <1686079252@qq.com>
2022-07-30 12:37:54 +0800

    Optimize part of the code, correct misspellings

--- include/memman.h
+++ include/memman.h
@@ -1,71 +1,72 @@
-#ifndef CC551FE8_D82A_487A_94B6_BB6D1175E56F
-#define CC551FE8_D82A_487A_94B6_BB6D1175E56F
+#ifndef _CMEMMAN_MEMMAN_H__
+#define _CMEMMAN_MEMMAN_H__
 
-#include <stdlib.h>
+#include <stddef.h>
 
 typedef void (*destructor_t)(void *);
 
 typedef struct _ptr_info {
-    void *ptr;
-    destructor_t destructor;
+	void *ptr;
+	destructor_t destructor;
 } ptr_info_t;
 
 typedef struct _memory_pool {
-    ptr_info_t **ptr_pool;
-    int scope_depth;
+	ptr_info_t **ptr_pool;
+	int scope_depth;
 } mempool_t;
 
 /**
  * Makes a memory pool.
  * @returns The memory pool object.
  */
-mempool_t* make_pool();
+mempool_t *make_pool();
 
 /**
  * Destroy a pool.
  * @param pool The pool to destroy.
  */
-void destroy_pool(mempool_t*);
+void destroy_pool(mempool_t *);
 
 /**
  * Enter a scope.
  * @param pool The pool.
  */
-void mp_enter_scope(mempool_t*);
+void mp_enter_scope(mempool_t *);
 
 /**
  * Borrow out the pointer of this scope and goto the previous scope.
  * @param pool The pool.
  * @param ptr The pointer to borrow out.
  */
-void mp_borrow_out(mempool_t*, void*);
+void mp_borrow_out(mempool_t *, void *);
 
 /**
  * Borrow in the pointer of the previous scope.
  * @param pool The pool.
  * @param ptr The pointer to borrow in.
  */
-void mp_borrow_in(mempool_t*, void*);
+void mp_borrow_in(mempool_t *, void *);
 
 /**
  * Exit the scope and free all the pointer of the scope.
  * @param pool The pool.
  */
-void mp_exit_scope(mempool_t*);
+void mp_exit_scope(mempool_t *);
 
 /**
  * Allocate a block of memory and add it to the current scope.
  */
-void *mp_malloc(mempool_t*, size_t);
+void *mp_malloc(mempool_t *, size_t);
 
 /**
  * Allocate a block of memory to the scope #1.
  */
-void *mp_malloc_outest(mempool_t*, size_t);
+void *mp_malloc_outest(mempool_t *, size_t);
 
 /**
- * Register a destructor for the given pointer. Notice that the pointer must be in the current scope.
+ * Register a destructor for the given pointer. Notice that the pointer must be
+ * in the current scope.
  */
-void mp_register_destructor(mempool_t*, void*, destructor_t);
+void mp_register_destructor(mempool_t *, void *, destructor_t);
 
-#endif /* CC551FE8_D82A_487A_94B6_BB6D1175E56F */
+#endif // _CMEMMAN_MEMMAN_H__
--- src/memman.c
+++ src/memman.c
@@ -1,188 +1,208 @@
 #include "../include/memman.h"
-#include <stdlib.h>
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define SELF mempool_t *self
 
-mempool_t* make_pool() {
-    // Make a new pool
-    mempool_t* pool = (mempool_t*) malloc(sizeof(mempool_t));
+mempool_t *make_pool() {
+	// Make a new pool
+	mempool_t *pool = (mempool_t *)malloc(sizeof(mempool_t));
 
-    // Initialize the scope depth
-    pool->scope_depth = 0;
+	// Initialize the scope depth
+	pool->scope_depth = 0;
 
-    // Initialize the pointer
-    pool->ptr_pool = NULL;
-    return pool;
+	// Initialize the pointer
+	pool->ptr_pool = NULL;
+	return pool;
 }
 
 void destroy_pool(SELF) {
-    // Exit scope
-    while (self->scope_depth) {
-        fprintf(stderr, "libcmemman: warning: scope %d never exits until destroying the pool.", self->scope_depth);
-        mp_exit_scope(self);
-    }
+	// Exit scope
+	while (self->scope_depth) {
+		fprintf(stderr,
+				"libcmemman: warning: scope %d never exits until destroying "
+				"the pool.",
+				self->scope_depth);
+		mp_exit_scope(self);
+	}
 
-    // Free the pool and the object itself.
-    free(self->ptr_pool);
-    free(self);
+	// Free the pool and the object itself.
+	assert(self != NULL && self->ptr_pool != NULL);
+	free(self->ptr_pool);
+	free(self);
 }
 
 void mp_enter_scope(SELF) {
 
-    #ifdef __DEBUG__
-    printf("Entering scope %d\n", self->scope_depth + 1);
-    #endif
+#ifdef __DEBUG__
+	printf("Entering scope %d\n", self->scope_depth + 1);
+#endif
 
-    // Entering the outest scope
-    if (self->scope_depth == 0) {
-        // Initialze the scope arrays
-        self->scope_depth = 1;
-        self->ptr_pool = (ptr_info_t**)malloc(sizeof(ptr_info_t*));
-        self->ptr_pool[0] = (ptr_info_t*)malloc(sizeof(ptr_info_t));
-        self->ptr_pool[0][0].ptr = NULL;
-    } else {
-        // Increase the scope arrays and add the element to the end of the array, i.e., push a new scope.
-        self->scope_depth++;
-        self->ptr_pool = (ptr_info_t**)realloc(self->ptr_pool, sizeof(ptr_info_t*) * self->scope_depth);
-        self->ptr_pool[self->scope_depth-1] = (ptr_info_t*)malloc(sizeof(ptr_info_t));
-        // Use NULL as the terminator.
-        self->ptr_pool[self->scope_depth-1][0].ptr = NULL;
-    }
+	// Entering the outest scope
+	if (self->scope_depth == 0) {
+		// Initialize the scope arrays
+		self->scope_depth = 1;
+		self->ptr_pool = (ptr_info_t **)malloc(sizeof(ptr_info_t *));
+		self->ptr_pool[0] = (ptr_info_t *)malloc(sizeof(ptr_info_t));
+		self->ptr_pool[0][0].ptr = NULL;
+	} else {
+		// Increase the scope arrays and add the element to the end of the
+		// array, i.e., push a new scope.
+		self->scope_depth++;
+		self->ptr_pool = (ptr_info_t **)realloc(
+			self->ptr_pool, sizeof(ptr_info_t *) * self->scope_depth);
+		self->ptr_pool[self->scope_depth - 1] =
+			(ptr_info_t *)malloc(sizeof(ptr_info_t));
+		// Use NULL as the terminator.
+		self->ptr_pool[self->scope_depth - 1][0].ptr = NULL;
+	}
 }
 
 int prv_mp_get_size_of_scope(SELF, int scope_id) {
-    int c = 0;
-    while(self->ptr_pool[scope_id][c].ptr) c++;
-    return c;
+	int c = 0;
+	while (self->ptr_pool[scope_id][c].ptr)
+		c++;
+	return c;
 }
 
-void prv_mp_add_ptr_to_scope(SELF, int scope_id, void* ptr) {
-    // Add pointer to the scope.
-    int len = prv_mp_get_size_of_scope(self, scope_id); 
-    self->ptr_pool[scope_id] = (ptr_info_t*)realloc(self->ptr_pool[scope_id], (len + 2) * sizeof(ptr_info_t));
-    self->ptr_pool[scope_id][len].ptr = ptr;
-    self->ptr_pool[scope_id][len].destructor = NULL;
-    self->ptr_pool[scope_id][len+1].ptr = NULL;
+void prv_mp_add_ptr_to_scope(SELF, int scope_id, void *ptr) {
+	// Add pointer to the scope.
+	int len = prv_mp_get_size_of_scope(self, scope_id);
+	self->ptr_pool[scope_id] = (ptr_info_t *)realloc(
+		self->ptr_pool[scope_id], (len + 2) * sizeof(ptr_info_t));
+	self->ptr_pool[scope_id][len].ptr = ptr;
+	self->ptr_pool[scope_id][len].destructor = NULL;
+	self->ptr_pool[scope_id][len + 1].ptr = NULL;
 }
 
 int prv_mp_get_index_of_ptr(SELF, int scope_id, void *ptr) {
-    // Search for the index of the ptr
-    int len = prv_mp_get_size_of_scope(self, scope_id);
-    for (int i = 0; i < len; i++) {
-        if (self->ptr_pool[scope_id][i].ptr == ptr) {
-            return i;
-        }
-    }
+	// Search for the index of the ptr
+	int len = prv_mp_get_size_of_scope(self, scope_id);
+	for (int i = 0; i < len; i++) {
+		if (self->ptr_pool[scope_id][i].ptr == ptr) {
+			return i;
+		}
+	}
 
-    // If cannot find it, return -1.
-    return -1;
+	// cannot find it
+	return -1;
 }
 
-void prv_mp_remove_ptr_from_scope(SELF, int scope_id, void* ptr) {
-    // Find the index of the pointer
-    int idx = prv_mp_get_index_of_ptr(self, scope_id, ptr);
-    int len = prv_mp_get_size_of_scope(self, scope_id);
-    // Cannot find it
-    if (idx == -1) {
-        fprintf(stderr, "libmemman: warning: trying to remove pointer (%p) that doesn't exist in scope %d", ptr, scope_id);
-        return;
-    }
+void prv_mp_remove_ptr_from_scope(SELF, int scope_id, void *ptr) {
+	// Find the index of the pointer
+	int idx = prv_mp_get_index_of_ptr(self, scope_id, ptr);
+	int len = prv_mp_get_size_of_scope(self, scope_id);
+	// Cannot find it
+	if (idx == -1) {
+		fprintf(stderr,
+				"libmemman: warning: trying to remove pointer (%p) that "
+				"doesn't exist in scope %d",
+				ptr, scope_id);
+		return;
+	}
 
-    // Delete the it
-    self->ptr_pool[scope_id][idx].ptr = NULL;
-    // L-Shift the whole array because we use NULL as terminator.
-    for (int i = idx; i < len; i++) {
-        self->ptr_pool[scope_id][i].ptr = self->ptr_pool[scope_id][i+1].ptr;
-        self->ptr_pool[scope_id][i].destructor = self->ptr_pool[scope_id][i+1].destructor;
-    }
+	// Delete it
+	self->ptr_pool[scope_id][idx].ptr = NULL;
+	// L-Shift the whole array because we use NULL as terminator.
+	for (int i = idx; i < len; i++) {
+		self->ptr_pool[scope_id][i].ptr = self->ptr_pool[scope_id][i + 1].ptr;
+		self->ptr_pool[scope_id][i].destructor =
+			self->ptr_pool[scope_id][i + 1].destructor;
+	}
 }
 
-void mp_borrow_out(SELF, void* ptr) {
-    if (self->scope_depth <= 1) {
-        return;
-    }
-    void *p = ptr;
-    // Borrow it out
-    prv_mp_remove_ptr_from_scope(self, self->scope_depth-1, p);
-    prv_mp_add_ptr_to_scope(self, self->scope_depth-2, p);
+void mp_borrow_out(SELF, void *ptr) {
+	if (self->scope_depth <= 1) {
+		return;
+	}
+	void *p = ptr;
+	// Borrow it out
+	prv_mp_remove_ptr_from_scope(self, self->scope_depth - 1, p);
+	prv_mp_add_ptr_to_scope(self, self->scope_depth - 2, p);
 }
 
 void mp_exit_scope(SELF) {
-    #ifdef __DEBUG__
-    printf("\nExiting scope %d\n", self->scope_depth);
-    #endif
+#ifdef __DEBUG__
+	printf("\nExiting scope %d\n", self->scope_depth);
+#endif
 
-    // Free all the pointers
-    int len = prv_mp_get_size_of_scope(self, self->scope_depth-1);
-    for (int i = 0; i < len; i++) {
+	// Free all the pointers
+	int len = prv_mp_get_size_of_scope(self, self->scope_depth - 1);
+	for (int i = 0; i < len; i++) {
 
-        #ifdef __DEBUG__
-        printf("Freeing %p\n", self->ptr_pool[self->scope_depth-1][i].ptr);
-        #endif
+#ifdef __DEBUG__
+		printf("Freeing %p\n", self->ptr_pool[self->scope_depth - 1][i].ptr);
+#endif
 
-        // If it has a destructor, then we use it.
-        if (self->ptr_pool[self->scope_depth-1][i].destructor != NULL) {
-            self->ptr_pool[self->scope_depth-1][i].destructor(self->ptr_pool[self->scope_depth-1][i].ptr);
-        }
-        // Free the pointer.
-        free(self->ptr_pool[self->scope_depth-1][i].ptr);
-    }
-    // Delete the last element, a.k.a. pop it
-    free(self->ptr_pool[self->scope_depth-1]);
-    self->scope_depth--;
-
+		// If it has a destructor, then we use it.
+		if (self->ptr_pool[self->scope_depth - 1][i].destructor != NULL) {
+			self->ptr_pool[self->scope_depth - 1][i].destructor(
+				self->ptr_pool[self->scope_depth - 1][i].ptr);
+		}
+		// Free the pointer.
+		free(self->ptr_pool[self->scope_depth - 1][i].ptr);
+	}
+	// Delete the last element, a.k.a. pop it
+	free(self->ptr_pool[self->scope_depth - 1]);
+	self->scope_depth--;
 }
 
 void mp_borrow_in(SELF, void *ptr) {
-    if (self->scope_depth <= 1) {
-        return;
-    }
-    void *p = ptr;
-    // Borrow it in
-    prv_mp_remove_ptr_from_scope(self, self->scope_depth-2, p);
-    prv_mp_add_ptr_to_scope(self, self->scope_depth-1, p);
+	if (self->scope_depth <= 1) {
+		return;
+	}
+	void *p = ptr;
+	// Borrow it in
+	prv_mp_remove_ptr_from_scope(self, self->scope_depth - 2, p);
+	prv_mp_add_ptr_to_scope(self, self->scope_depth - 1, p);
 }
 
 void *mp_malloc(SELF, size_t size) {
-    if (self->scope_depth == 0) {
-        fprintf(stderr, "libcmemman: fatal: trying to allocate memory without enterning any scope.");
-        destroy_pool(self);
-        exit(-1);
-    }
-    // Malloc and add it into the scope.
-    void *result = malloc(size);
-    if (!result) {
-        // If failed, just return NULL and do nothing because we needn't to register NULL.
-        return NULL;
-    }
-    prv_mp_add_ptr_to_scope(self, self->scope_depth-1, result);
-    return result;
+	if (self->scope_depth == 0) {
+		fprintf(stderr, "libcmemman: fatal: trying to allocate memory without "
+						"entering any scope.");
+		destroy_pool(self);
+		exit(-1);
+	}
+	// Malloc and add it into the scope.
+	void *result = malloc(size);
+	if (!result) {
+		// If failed, just return NULL and do nothing because we needn't
+		// register NULL.
+		return NULL;
+	}
+	prv_mp_add_ptr_to_scope(self, self->scope_depth - 1, result);
+	return result;
 }
 
 void *mp_malloc_outest(SELF, size_t size) {
-    if (self->scope_depth == 0) {
-        fprintf(stderr, "libcmemman: fatal: trying to allocate memory without enterning any scope.");
-        destroy_pool(self);
-        exit(-1);
-    }
-    void *result = malloc(size);
-    if (!result) {
-        // Failed, no need to register
-        return NULL;
-    }
-    prv_mp_add_ptr_to_scope(self, 0, result);
-    return result;
+	if (self->scope_depth == 0) {
+		fprintf(stderr, "libcmemman: fatal: trying to allocate memory without "
+						"entering any scope.");
+		destroy_pool(self);
+		exit(-1);
+	}
+	void *result = malloc(size);
+	if (!result) {
+		// Failed, no need to register
+		return NULL;
+	}
+	prv_mp_add_ptr_to_scope(self, 0, result);
+	return result;
 }
 
-void mp_register_destructor(SELF, void* ptr, destructor_t dest) {
-    // Get the index of the pointer
-    int idx = prv_mp_get_index_of_ptr(self, self->scope_depth-1, ptr);
-    if (idx == -1) {
-        fprintf(stderr, "libmemman: warning: trying to remove pointer (%p) that doesn't exist in scope %d", ptr, self->scope_depth-1);
-        return;
-    }
-    
-    // Register the destructor
-    self->ptr_pool[self->scope_depth-1][idx].destructor = dest;
+void mp_register_destructor(SELF, void *ptr, destructor_t dest) {
+	// Get the index of the pointer
+	int idx = prv_mp_get_index_of_ptr(self, self->scope_depth - 1, ptr);
+	if (idx == -1) {
+		fprintf(stderr,
+				"libmemman: warning: trying to remove pointer (%p) that "
+				"doesn't exist in scope %d",
+				ptr, self->scope_depth - 1);
+		return;
+	}
+
+	// Register the destructor
+	self->ptr_pool[self->scope_depth - 1][idx].destructor = dest;
 }
